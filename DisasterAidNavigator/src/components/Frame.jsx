90214affffcfdd8238d9a3dd866ceb029e278b7d
krishitapandey <pandeykrishita@gmail.com>
2024-11-22 13:25:38 +0545

    UI

--- DisasterAidNavigator/src/components/Frame.jsx
+++ DisasterAidNavigator/src/components/Frame.jsx
@@ -1,38 +1,98 @@
 import "../App.css";
-import map from "../assets/map.png";
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import call from "../assets/call.png";
+import hospital from "../assets/hospital.png";
+import prone from "../assets/prone.png";
 
 function Frame() {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const openPopup = () => setIsPopupVisible(true);
+  const closePopup = () => setIsPopupVisible(false);
+
   return (
-    <div className="frame-container " >
-      <div className="info-box">
-        <div className="medical-attention">
-          <img src={map} alt="Map" className="map-image" />
-          <div className="medical-text">Medical Attention</div>
-          <div className="button" >
-          <Link  to={"/hospital"} >Click Here</Link>
-          </div>
+    <>
+      <div className="alert-background">
+        <div className="alert-icon" style={{ top: "10%", left: "20%" }}>
+          !
+        </div>
+        <div className="alert-icon" style={{ top: "40%", left: "10%" }}>
+          !
+        </div>
+        <div className="alert-icon" style={{ top: "30%", left: "20%" }}>
+          !
+        </div>
+        <div className="alert-icon" style={{ top: "90%", left: "20%" }}>
+          !
+        </div>
+        <div className="alert-icon" style={{ top: "50%", left: "70%" }}>
+          !
+        </div>
+        <div className="alert-icon" style={{ top: "80%", left: "40%" }}>
+          !
+        </div>
+        <div className="alert-icon" style={{ top: "50%", left: "70%" }}>
+          !
+        </div>
+        <div className="alert-icon" style={{ top: "40%", left: "60%" }}>
+          !
+        </div>
+        <div className="alert-icon" style={{ top: "20%", left: "70%" }}>
+          !
+        </div>
+        <div className="alert-icon" style={{ top: "60%", left: "40%" }}>
+          !
         </div>
       </div>
-      <div className="info-box">
-        <div className="medical-attention">
-          <img src={map} alt="Map" className="map-image" />
-          <div className="medical-text">Medical Attention</div>
-          <div className="button" >
-          <Link  to={"/disaster"} >Click Here</Link>
+
+      <h2 className="frame-title">Stay Prepared. Stay Safe. Act Fast.</h2>
+
+      <div className="frame-container ">
+        <div className="info-box">
+          <div className="medical-attention">
+            <img src={hospital} alt="Map" className="map-image" />
+            <div className="medical-text">Find Nearby Hospitals</div>
+            <div className="button">
+              <Link to={"/hospital"}> Locate Hospitals</Link>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="info-box">
-        <div className="medical-attention">
-          <img src={map} alt="Map" className="map-image" />
-          <div className="medical-text">Medical Attention</div>
-          <div className="button" >
-          <Link  to={"/hospitals"} >Click Here</Link>
+        <div className="info-box">
+          <div className="medical-attention">
+            <img src={prone} alt="Map" className="map-image" />
+            <div className="medical-text">View Risk-Prone Areas</div>
+            <div className="button">
+              <Link to={"/disaster"}> View Map</Link>
+            </div>
           </div>
         </div>
+        <div className="info-box">
+          <div className="medical-attention">
+            <img src={call} alt="Map" className="map-image" />
+            <div className="medical-text">Call Nearby Rescuer</div>
+            <div className="call">
+              <button id="open-popup " className="button" onClick={openPopup}>
+                Call Rescuer
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* Popup component */}
+        {isPopupVisible && (
+          <div className="popup-overlay">
+            <div className="popup">
+              <div className="close-btn" onClick={closePopup}>
+                &times;
+              </div>
+              <h1>Sorry, can not call from the web</h1>
+              <p>Please use the mobile app to make a call to the rescuer.</p>
+            </div>
+          </div>
+        )}
       </div>
-    </div>
+    </>
   );
 }
 
--- DisasterAidNavigator/src/components/Home.jsx
+++ DisasterAidNavigator/src/components/Home.jsx
@@ -2,24 +2,28 @@ import "../App.css";
 import logo from "../assets/logo2.png";
 import ChatInterface from "./Chat";
 import Frame from "./Frame";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <>
-    <div className="container">
-      <div className="logo">
-        <img src={logo} alt="Logo" />
+      <div className="container">
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="website-name">
+          <h1>The DisasterAid Navigator</h1>
+        </div>
+        <div className="slogan">
+          <p>Your guide to disaster relief and recovery.</p>
+        </div>
+
+        <div className="button1">
+          <Link to={"/frame"}> Do you need help</Link>
+        </div>
       </div>
-      <div className="website-name">
-        <h1>The DisasterAid Navigator</h1>
-      </div>
-      <div className="slogan">
-        <p>Your guide to disaster relief and recovery.</p>
-      </div>
-      <div className="button1">Do you need help ?</div>
-    </div>
-    <Frame />
-    <ChatInterface />
+      <Frame />
+      <ChatInterface />
     </>
   );
 }
